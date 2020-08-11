9368a31100853113013af176c2e9b0eb1db31b11
ms0130k <minseok0130@gmail.com>
2020-08-12 01:00:00 +0900

    c chapter2

---
+++ Practice/Practice/Comment01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+이 구간의 코드는 모두 주석문입니다.
+*/
+
+int main(void)
+{
+	//결과를 저장하는 변수입니다.
+	int nResult = 10;
+	char ch = 'A'; //사용자 입력
+
+	//printf("Hello, World\n");
+	return 0;
+}
