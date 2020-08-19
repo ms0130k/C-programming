afc60dedc1002ec2df0b2084a426264cfc23a9ac
ms0130k <minseok0130@gmail.com>
2020-08-20 02:27:49 +0900

    c programming

---
+++ Practice/Practice/exercise_03-01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int nAge = 0;
+	char szName[5] = { 0 };
+
+	printf("나이를 입력하세요. : ");
+	scanf_s("%d", nAge);
+}
---
+++ Practice/Practice/floatprint01.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("%d\n", sizeof(123.456F));
+//	printf("%d\n", sizeof(123.456));
+//
+//	printf("%f\n", 123.456F);
+//
+//	printf("%f\n", 123.456);
+//	printf("%lf\n", 123.456);
+//}
