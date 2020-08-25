c47029202a81c78a75f402efc908c9255ca6b58f
ms0130k <minseok0130@gmail.com>
2020-08-26 02:16:42 +0900

    연산자 기본

---
+++ Practice/Practice/TypePromotion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char ch = 'A';
+
+	printf("%c\n", ch);
+	printf("%c\n", ch + 1);
+	printf("%c\n", 'A' + 2);
+
+	printf("%d\n", 5.0 + 2);//'double + int'는 double 형식이므로 %d 형식으로 출력할 수 없다.
+	printf("%f\n", 5.0 + 2);
+
+	return 0;
+}
