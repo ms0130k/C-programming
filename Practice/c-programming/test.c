672def4dba6ebc586bece88fafb3efc2d8a36191
ms0130k <minseok0130@gmail.com>
2020-10-08 16:05:42 +0900

    c-practice

---
+++ Practice/c-programming/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+	int nData = 3000;
+	nData = 7 + 3;
+	printf("%d", nData);
+}
---
+++ c-programming/c-programming/c-programming/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(void) {
+	int nData = 300;
+	nData = 7 + 3;
+	printf("%d", nData);
+}
